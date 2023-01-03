d0b9957ad7adf594c042059253b59b44fd0fd271
Лилия <l_loshakova123@mail.ru>
2023-01-03 19:52:07 +0300

    Sketch

--- Human.java
+++ Human.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 public class Human {
     String name;
-    String gender;
     Human father;
     Human mather;
     Human Grandma;
     Human Grandpa;
     List<Human> children;
 
-    
+    public Human(String name, Human father){
+        this.name = name;
+        this.father = father;
+        father.children.add(this);
+
+    }
+
 }
