77c38e31ea5e84d017cdb8c8794b60a9b768cc6c
Лилия <l_loshakova123@mail.ru>
2023-01-05 23:52:35 +0300

    Sketch

--- FamilyTree.java
+++ FamilyTree.java
@@ -8,15 +8,19 @@ public class FamilyTree {
 
     public void preOrder(Human tree, String spase){
         if(tree != null)
-        System.out.println(spase + tree.name);
+        System.out.println(spase + tree.getName());
         else{
             System.out.println(spase + "nil");
             return;
         }
 
-        for(int i = 0; i < tree.family.size(); i++){
-            preOrder(tree.family.get(i).getH2(), spase + " ");
+        for(int i = 0; i < tree.getFamily().size(); i++){
+            preOrder(tree.getFamily().get(i).getH2(), spase + " ");
         }
+
+        // for(int i = 0; i < tree.family.size(); i++){
+        //     preOrder(tree.family.get(i).getH2(), spase + " ");
+        // }
     }
     
     // public FamilyTree(List<Human> personList) {
