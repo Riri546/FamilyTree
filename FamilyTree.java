a3479b72bb6f7a19ecc6fd0eb6f3e40a0c35c210
Лилия <l_loshakova123@mail.ru>
2023-01-06 00:01:24 +0300

    Sketch

--- FamilyTree.java
+++ FamilyTree.java
@@ -27,23 +27,7 @@ public class FamilyTree {
     //    this.personList = personList;
     // }
     
-    // public FamilyTree() {
-    //     this(new ArrayList<>());
-    // }
-    
-    // //*Для того что бы моно было читать инфо из приватной переменной, а точнее в листе*/
-    // public List<Human> getPersonList() {
-    //     return personList;
-    // }
 
-    // //*Для того что бы модно было изменять данные в приватной переменной, а точнее в листе */
-    // public void setProductList(List<Human> personList) {
-    //     this.personList = personList;
-    // }
-
-    // //*Для сохранения новых данных в лист */
-    // public void addPerson(Human person){
-    //   
 
 }
 
