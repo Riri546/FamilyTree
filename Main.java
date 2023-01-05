be866b5921562cd9e8046788f003b080fa9d51b4
Лилия <l_loshakova123@mail.ru>
2023-01-06 00:00:25 +0300

    Sketch

--- Main.java
+++ Main.java
@@ -16,35 +16,6 @@ public class Main {
 
         h1.printRelatives(2);
 
-        tree.preOrder(h1,"");
+        tree.preOrder(h1, "");
     }
-
-
-    // public static void main(String[] args) {
-        // FamilyTree family = new FamilyTree();
-
-
-        // Human person1 = new Human("Юрий", 59);
-        // Human person2 = new Human("Рита", 57);
-        // Human person3 = new Human("Станислав", 40);
-        // Human person4 = new Human("Юлия", 37);
-        // Human person5 = new Human("Лилия", 21);
-        // Human person6 = new Human("Виталий", 12);
-
-        // family.addPerson(person1);
-        // family.addPerson(person2);
-        // family.addPerson(person3);
-        // family.addPerson(person4);
-        // family.addPerson(person5);
-        // family.addPerson(person6);
-
-        // person4 = new Human(person1, person2);
-        // person5 = new Human(person1, person2);
-        // person6 = new Human(person3, person4);
-
-        
-
-        // System.out.println(family.getFamily("Виталий"));
-    }
-// }
-
+}
