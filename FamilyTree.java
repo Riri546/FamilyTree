bfe8015e22c36379de7029998f90b3e85c12f1b9
Лилия <l_loshakova123@mail.ru>
2023-01-05 19:19:36 +0300

    Sketch

--- FamilyTree.java
+++ FamilyTree.java
@@ -1,42 +1,23 @@
 package FamilyTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FamilyTree {
     
-    // import java.util.ArrayList;
-    // import java.util.List;
+    private List<Human> person;
     
-    // public class VendingMachine {
-    //     private List<Product> productList;
-    
-    //     public VendingMachine(List<Product> productList) {
-    //         this.productList = productList;
-    //     }
-    
-    //     public VendingMachine() {
-    //         this(new ArrayList<>());
-    //     }
-    
-    //     public List<Product> getProductList() {
-    //         return productList;
-    //     }
-    
-    //     public void setProductList(List<Product> productList) {
-    //         this.productList = productList;
-    //     }
-    
-    //     public void addProduct(Product product){
-    //         productList.add(product);
-    //     }
-    
-    //     public List<Product> getProductByCost(int cost){
-    //         List<Product> res = new ArrayList<>();
-    //         for (Product product: productList){
-    //             if (product.getCost() <= cost){
-    //                 res.add(product);
-    //             }
-    //         }
-    //         return res;
-    //     }
+    public FamilyTree(List<Human> person) {
+       this.person = person;
     }
-
+    
+    public FamilyTree() {
+        this(new ArrayList<>());
+    }
+    
+    public List<Human> getPerson() {
+        return person;
+    }
 }
+
+
