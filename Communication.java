c6c16113ce3819553e5ad639a990d66179ebd069
Лилия <l_loshakova123@mail.ru>
2023-01-05 23:17:39 +0300

    Sketch

--- Communication.java
+++ Communication.java
@@ -1,7 +1,5 @@
 package FamilyTree;
 
-import java.util.ArrayList;
-
 public class Communication {
 
     private Human h1;
@@ -9,29 +7,26 @@ public class Communication {
     private int degreeKinship1;
     private int degreeKinship2;
 
-    public Human getH1(){
+    public Human getH1() {
         return h1;
     }
 
-    public Human getH2(){
+    public Human getH2() {
         return h2;
     }
 
-    public int getDegreeKinship1(){
+    public int getDegreeKinship1() {
         return degreeKinship1;
     }
 
-    public int getDegreeKinship2(){
+    public int getDegreeKinship2() {
         return degreeKinship2;
     }
-    public Communication(Human h1, Human h2, int degreeKinship1, int degreeKinship2){
+
+    public Communication(Human h1, Human h2, int degreeKinship1, int degreeKinship2) {
         this.h1 = h1;
         this.h2 = h2;
         this.degreeKinship1 = degreeKinship1;
         this.degreeKinship2 = degreeKinship2;
     }
-
-
-
-
 }
