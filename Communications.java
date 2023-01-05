830f9e618b3bbe925a33bc7d910eaa0664d17dfa
Лилия <l_loshakova123@mail.ru>
2023-01-05 23:57:55 +0300

    Sketch

--- Communications.java
+++ Communications.java
@@ -4,10 +4,8 @@ public class Communications {
 
     private Human h1;
     private Human h2;
-    // private int degreeKinship1;
-    // private int degreeKinship2;
-    private String degreeKinship1;
-    private String degreeKinship2;
+    private int degreeKinship1;
+    private int degreeKinship2;
 
     public Human getH1() {
         return h1;
@@ -17,34 +15,18 @@ public class Communications {
         return h2;
     }
 
-    public String getDegreeKinship1() {
+    public int getDegreeKinship1() {
         return degreeKinship1;
     }
 
-    public String getDegreeKinship2() {
+    public int getDegreeKinship2() {
         return degreeKinship2;
     }
 
-    // public int getDegreeKinship1() {
-    //     return degreeKinship1;
-    // }
-
-    // public int getDegreeKinship2() {
-    //     return degreeKinship2;
-    // }
-
-
-    public Communications(Human h1, Human h2, String degreeKinship1, String degreeKinship2) {
+    public Communications(Human h1, Human h2, int degreeKinship1, int degreeKinship2) {
         this.h1 = h1;
         this.h2 = h2;
         this.degreeKinship1 = degreeKinship1;
         this.degreeKinship2 = degreeKinship2;
     }
-
-    // public Communications(Human h1, Human h2, int degreeKinship1, int degreeKinship2) {
-    //     this.h1 = h1;
-    //     this.h2 = h2;
-    //     this.degreeKinship1 = degreeKinship1;
-    //     this.degreeKinship2 = degreeKinship2;
-    // }
 }
