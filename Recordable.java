20762609273671e4ce32ca0a85f2939985cf94f7
Лилия <l_loshakova123@mail.ru>
2023-01-06 19:34:01 +0300

    Sketch

--- Recordable.java
+++
@@ -1,5 +0,0 @@
-package FamilyTree;
-
-public interface Recordable {
-    void save();
-}
--- Recorded.java
+++ Recorded.java
@@ -2,29 +2,27 @@ package FamilyTree;
 
 import java.io.*;
 
-    public class Recorded implements Serializable {
-        private String name;
-        private String contact;
+public class Recorded implements Serializable {
+    private String name;
+    private String contact;
 
-        public Recorded(String name, String contact) {
-            this.name = name;
-            this.contact = contact;
-        }
+    public Recorded(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
 
-        @Override
-        public String toString() {
-            return "Logon{" +
-                    "name='" + name + '\'' +
-                    ", contact='" + contact + '\'' +
-                    '}';
-        }
-
-    
+    @Override
+    public String toString() {
+        return "Logon{" +
+                "name='" + name + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Recorded igor = new Recorded("IgorIvanovich", "Khoziain");
         Recorded renat = new Recorded("Renat", "2500RUB");
-       
+
         System.out.println("Before: \n" + igor);
         System.out.println(renat);
 
@@ -41,10 +39,3 @@ import java.io.*;
         System.out.println(renat);
     }
 }
-    // public void save() {
-    // Human h1 = new Human("Коля", 24);
-    // Human h2 = new Human("Даша", 53);
-    // Human h3 = new Human("Витя", 65);
-
-    // }
-
