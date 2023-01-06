2a415eb4af24669e4169f4f59e72c646f8a7d7f3
Лилия <l_loshakova123@mail.ru>
2023-01-06 19:39:42 +0300

    Sketch

--- Recorded.java
+++ Recorded.java
@@ -4,31 +4,38 @@ import java.io.*;
 
 public class Recorded implements Serializable {
     private String name;
+    private int age;
     private String contact;
 
-    public Recorded(String name, String contact) {
+    public Recorded(String name,int age, String contact) {
         this.name = name;
+        this.age = age;
         this.contact = contact;
+        
     }
 
     @Override
     public String toString() {
         return "Logon{" +
-                "name='" + name + '\'' +
-                ", contact='" + contact + '\'' +
+                "name ='" + name + '\'' +
+                ", age ='" + age + '\'' +
+                ", contact ='" + contact + '\'' +
                 '}';
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Recorded igor = new Recorded("IgorIvanovich", "Khoziain");
-        Recorded renat = new Recorded("Renat", "2500RUB");
+        Recorded h1 = new Recorded("Коля", 24, "Сын Даши и Вити");
+        Recorded h2 = new Recorded("Даша", 53, "Мать Коли");
+        Recorded h3 = new Recorded("Витя", 65, "Отец Коли");
 
-        System.out.println("Before: \n" + igor);
-        System.out.println(renat);
+        System.out.println(h1);
+        System.out.println(h2);
+        System.out.println(h3);
 
         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Externals.out"));
-        out.writeObject(igor);
-        out.writeObject(renat);
+        out.writeObject(h1);
+        out.writeObject(h2);
+        out.writeObject(h3);
         out.close();
 
         ObjectInputStream in = new ObjectInputStream(new FileInputStream("Externals.out"));
