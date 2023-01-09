b50fc05cfcb0f7d9f469172a51a6a17e313feff8
Лилия <l_loshakova123@mail.ru>
2023-01-10 00:57:57 +0300

    Sketch

--- Human.java
+++ Human.java
@@ -9,32 +9,34 @@ public class Human implements Comparable<Human> {
     private int age;
     private Human father;
     private Human mather;
+    private Human grandfather;
+    private Human grandmather;
     private List<Human> children;
-
-    public Human(String name, int age, Human father, Human mather) {
-        this.name = name;
-        this.age = age;
-        this.father = father;
-        this.mather = mather;
-        if (father != null) {
-            father.children.add(this);
-        }
-        if (mather != null) {
-            mather.children.add(this);
-        }
-    }
 
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    public Human(String name, int age, Human father, Human mather) {
+        this.name = name;
+        this.age = age;
+        this.father = father;
+        this.mather = mather;
+        // if (father != null) {
+        // father.children.add(this);
+        // }
+        // if (mather != null) {
+        // mather.children.add(this);
+        // }
+    }
+
     @Override
     public String toString() {
         if (father == null & mather == null) {
-            return "Имя: " + name + " Возрост: " + age;
+            return " " + name + " " + age;
         } else {
-            return "Имя: " + name + " Возрост: " + age + " Отец: " + father + " Мать: " + mather;
+            return " " + name + " " + age + " Отец: " + father + " Мать: " + mather;
         }
     }
 
@@ -81,5 +83,21 @@ public class Human implements Comparable<Human> {
 
     public void setMather(Human mather) {
         this.mather = mather;
+    }
+
+    public Human getGrandfather() {
+        return grandfather;
+    }
+
+    public void setGrandfather(Human grandfather) {
+        this.grandfather = grandfather;
+    }
+
+    public Human getGrandmather() {
+        return grandmather;
+    }
+
+    public void setGrandmather(Human grandmather) {
+        this.grandmather = grandmather;
     }
 }
--- Main.java
+++ Main.java
@@ -5,22 +5,25 @@ public class Main {
         FamilyTree tree = new FamilyTree();
         Service service = new Service(tree);
 
-        Human h1 = new Human("Мария", 54, null, null);
+        Human h1 = new Human("Мария", 54);
         tree.addHuman(h1);
-        Human h2 = new Human("Валерий", 58, null, null);
+        Human h2 = new Human("Валерий", 58);
         tree.addHuman(h2);
         Human h3 = new Human("Дмитрий", 31, h2, h1);
         tree.addHuman(h3);
-        Human h4 = new Human("Дарья", 29, null, null);
-        tree.addHuman(h4);
-        Human h5 = new Human("София", 10, h3, h4);
-        tree.addHuman(h5);
+        //Сейчас при выводе h5 помимо отца и матери выводятся бабушка и дедушка, но он
+        // Human h4 = new Human("Дарья", 29);
+        // tree.addHuman(h4);
+        // Human h5 = new Human("София", 10, h3, h4);
+        // tree.addHuman(h5);
 
+        System.out.println("До сортировки: ");
         for (Human human : tree) {
             System.out.println(human);
         }
 
         System.out.println();
+        System.out.println("После сортировки по имени: ");
         service.sortByName();
 
         for (Human human : tree) {
