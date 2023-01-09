f7218d4234385a8ad1fc940166b0a0d28f044578
Лилия <l_loshakova123@mail.ru>
2023-01-09 23:48:15 +0300

    New file

--- Main.java
+++ Main.java
@@ -5,15 +5,15 @@ public class Main {
         FamilyTree tree = new FamilyTree();
 
         Human h1 = new Human("Мария", 54, null, null);
-        tree.treeHuman.add(h1);
+        tree.addHuman(h1);
         Human h2 = new Human("Валерий", 58, null, null);
-        tree.treeHuman.add(h2);
+        tree.addHuman(h2);
         Human h3 = new Human("Дмитрий", 31, h2, h1);
-        tree.treeHuman.add(h3);
+        tree.addHuman(h3);
         Human h4 = new Human("Дарья", 29, null, null);
-        tree.treeHuman.add(h4);
+        tree.addHuman(h4);
         Human h5 = new Human("София", 10, h3, h4);
-        tree.treeHuman.add(h5);
+        tree.addHuman(h5);
 
         // Human h1 = new Human("Коля", 24);
         // tree.treeHuman.add(h1);
