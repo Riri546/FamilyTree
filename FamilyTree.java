e3828f7a84e2b67f0d88a699980ea957712d17ba
Лилия <l_loshakova123@mail.ru>
2023-01-09 23:43:22 +0300

    Sketch

--- FamilyTree.java
+++ FamilyTree.java
@@ -1,21 +1,45 @@
 package FamilyTree;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class FamilyTree {
+public class FamilyTree implements Iterable<Human> {
+    private ArrayList<Human> treeHuman;
 
-    ArrayList<Human> treeHuman = new ArrayList<Human>();
+    public FamilyTree(){
 
-    public void preOrder(Human tree, String spase) {
-        if (tree != null)
-            System.out.println(spase + tree.getName());
-        else {
-            System.out.println(spase + "nil");
-            return;
-        }
-
-        for (int i = 0; i < tree.getFamily().size(); i++) {
-            preOrder(tree.getFamily().get(i).getH2(), spase + "");
-        }
     }
+
+    
+        public StudyGroup() {
+            studentList = new ArrayList<>();
+        }
+    
+        public void addStudent(Student student) {
+            studentList.add(student);
+        }
+    
+        @Override
+        public Iterator<Student> iterator() {
+            return new StudyGroupIterator(studentList);
+        }
+    
+        public List<Student> getStudentList() {
+            return studentList;
+        }
+
+
+    // public void preOrder(Human tree, String spase) {
+    //     if (tree != null)
+    //         System.out.println(spase + tree.getName());
+    //     else {
+    //         System.out.println(spase + "nil");
+    //         return;
+    //     }
+
+    //     for (int i = 0; i < tree.getFamily().size(); i++) {
+    //         preOrder(tree.getFamily().get(i).getH2(), spase + "");
+    //     }
+    // }
 }
