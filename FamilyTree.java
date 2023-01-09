fa6285b98fbfb53a694102a0991028ec80a6891f
Лилия <l_loshakova123@mail.ru>
2023-01-09 23:51:17 +0300

    Sketch

--- FamilyTree.java
+++ FamilyTree.java
@@ -7,36 +7,33 @@ import java.util.List;
 public class FamilyTree implements Iterable<Human> {
     private ArrayList<Human> treeHuman;
 
-    public FamilyTree(){
+    public FamilyTree() {
         treeHuman = new ArrayList<>();
     }
 
     public void addHuman(Human human) {
         treeHuman.add(human);
     }
-    
-    // @Override
-    // public Iterator<Human> iterator() {
-    //     return new StudyGroupIterator(treeHuman);
-    // }
-    
-    // public List<Human> getTreeHuman() {
-    //     return treeHuman;
-    // }
 
+    @Override
+    public Iterator<Human> iterator() {
+    return new FamilyTreeIterator(treeHuman);
+    }
 
-    
+    public List<Human> getTreeHuman() {
+    return treeHuman;
+    }
 
     // public void preOrder(Human tree, String spase) {
-    //     if (tree != null)
-    //         System.out.println(spase + tree.getName());
-    //     else {
-    //         System.out.println(spase + "nil");
-    //         return;
-    //     }
+    // if (tree != null)
+    // System.out.println(spase + tree.getName());
+    // else {
+    // System.out.println(spase + "nil");
+    // return;
+    // }
 
-    //     for (int i = 0; i < tree.getFamily().size(); i++) {
-    //         preOrder(tree.getFamily().get(i).getH2(), spase + "");
-    //     }
+    // for (int i = 0; i < tree.getFamily().size(); i++) {
+    // preOrder(tree.getFamily().get(i).getH2(), spase + "");
+    // }
     // }
 }
--- FamilyTreeIterator.java
+++ FamilyTreeIterator.java
@@ -1,5 +1,24 @@
 package FamilyTree;
 
-public class FamilyTreeIterator {
-    
+import java.util.Iterator;
+import java.util.List;
+
+public class FamilyTreeIterator implements Iterator<Human> {
+
+    private int index;
+    private List<Human> treeHuman;
+
+    public FamilyTreeIterator (List<Human> treeHuman) {
+            this.treeHuman = treeHuman;
+        }
+
+    @Override
+    public boolean hasNext() {
+        return index < treeHuman.size();
+    }
+
+    @Override
+    public Human next() {
+        return treeHuman.get(index++);
+    }
 }
