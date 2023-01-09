ce4fd86b361b49e0b4aa0e64caf00a9ff58aee97
Лилия <l_loshakova123@mail.ru>
2023-01-10 00:45:55 +0300

    Sketch

--- Main.java
+++ Main.java
@@ -26,21 +26,5 @@ public class Main {
         for (Human human : tree) {
             System.out.println(human);
         }
-
-        // System.out.println();
-
-        // Human h1 = new Human("Коля", 24);
-        // tree.treeHuman.add(h1);
-        // Human h2 = new Human("Даша", 53);
-        // tree.treeHuman.add(h2);
-        // Human h3 = new Human("Витя", 65);
-        // tree.treeHuman.add(h3);
-
-        // h1.addCommunications(h2, "Дети", "Родители");
-        // h1.addCommunications(h3, "Дети", "Родители");
-
-        // h1.toString("Родители");
-
-        // tree.preOrder(h1, "");
     }
 }
