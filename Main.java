df6c5901315c417f884ce1d1a0964b54ec4b8f96
Лилия <l_loshakova123@mail.ru>
2023-01-17 03:19:33 +0300

    Sketch

--- Main.java
+++ Main.java
@@ -2,45 +2,7 @@ package FamilyTree;
 
 public class Main {
     public static void main(String[] args) {
-        FamilyTree<Human> tree = new FamilyTree<>();
         View view = new View();
-
-        Human h1 = new Human("Мария", 54);
-        tree.addHuman(h1);
-        Human h2 = new Human("Валерий", 58);
-        tree.addHuman(h2);
-        Human h3 = new Human("Дмитрий", 31, h2, h1);
-        tree.addHuman(h3);
-        Human h4 = new Human("Дарья", 29);
-        tree.addHuman(h4);
-        // Сейчас при выводе h5 помимо отца и матери,выводятся отец отца и мать отца и
-        // плюсом они же как бабушка с дедушкой
-        Human h5 = new Human("София", 10, h3, h4, h2, h1);
-        tree.addHuman(h5);
-
         view.communicationUse();
-
-        
-
-        // System.out.println("До сортировки: ");
-        // for (Human human : tree) {
-        //     System.out.println(human);
-        // }
-
-        // System.out.println();
-        // System.out.println("После сортировки по имени: ");
-        // service.sortByName();
-
-        // for (Human human : tree) {
-        //     System.out.println(human);
-        // }
-
-        // System.out.println();
-        // System.out.println("После сортировки по возрасту: ");
-        // service.sortByAge();
-
-        // for (Human human : tree) {
-        //     System.out.println(human);
-        // }
     }
 }
--- View.java
+++ View.java
@@ -5,10 +5,22 @@ import java.util.Scanner;
 public class View {
     FamilyTree<Human> tree = new FamilyTree<>();
     Service service = new Service(tree);
-
     Scanner scanner = new Scanner(System.in);
 
     public void communicationUse() {
+        Human h1 = new Human("Мария", 54);
+        tree.addHuman(h1);
+        Human h2 = new Human("Валерий", 58);
+        tree.addHuman(h2);
+        Human h3 = new Human("Дмитрий", 31, h2, h1);
+        tree.addHuman(h3);
+        Human h4 = new Human("Дарья", 29);
+        tree.addHuman(h4);
+        // Сейчас при выводе h5 помимо отца и матери,выводятся отец отца и мать отца и
+        // плюсом они же как бабушка с дедушкой
+        Human h5 = new Human("София", 10, h3, h4, h2, h1);
+        tree.addHuman(h5);
+
         System.out.println("Здравствуйте! Это генеологическое древо.");
         System.out.println(
                 "В каком порядке хотите просмотреть список людей?" + "\n" +
@@ -23,25 +35,26 @@ public class View {
 
         if (numberUser == 1) {
             System.out.println("Пожалуйста! Как вы и просили список людей из семейного древа: ");
-            System.out.println();
 
             for (Human human : tree) {
                 System.out.println(human);
             }
         }
-        if(numberUser == 2) {
+        if (numberUser == 2) {
             System.out.println("Это древо отсортировано в алфавитном порядке по имени: ");
-            System.out.println();
-
             service.sortByName();
-            for (Human human : tree) {
-                    System.out.println(human);
-                }
-        }
-        if(numberUser == 3){
-            System.out.println("Это древо представленно в виде отсортированного по возрасту: ");
-            System.out.println();
 
+            for (Human human : tree) {
+                System.out.println(human);
+            }
+        }
+        if (numberUser == 3) {
+            System.out.println("Это древо представленно в виде отсортированного по возрасту: ");
+            service.sortByAge();
+
+            for (Human human : tree) {
+                System.out.println(human);
+            }
 
         }
 
