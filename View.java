81bc10ef4722de93d80acc75f785aaeea74a222b
Лилия <l_loshakova123@mail.ru>
2023-01-17 03:13:02 +0300

    Sketch

--- View.java
+++ View.java
@@ -9,12 +9,12 @@ public class View {
     Scanner scanner = new Scanner(System.in);
 
     public void communicationUse() {
-        System.out.println("Здравствуйте! Это генеологическое древо");
+        System.out.println("Здравствуйте! Это генеологическое древо.");
         System.out.println(
                 "В каком порядке хотите просмотреть список людей?" + "\n" +
                         "1 - Вывести полный список людей из семейного древа" + "\n" +
-                        "2 - Отсортировать полный список в алфавитном порядке" + "\n" +
-                        "3 - Отсортировать по возросту(в порядке возрастания)");
+                        "2 - Отсортировать полный список по имени в алфавитном порядке" + "\n" +
+                        "3 - Отсортировать по возросту в порядке возрастания");
 
         System.out.println();
         System.out.print("Введите цифру для выполнения действия: ");
@@ -24,10 +24,25 @@ public class View {
         if (numberUser == 1) {
             System.out.println("Пожалуйста! Как вы и просили список людей из семейного древа: ");
             System.out.println();
-            
+
             for (Human human : tree) {
                 System.out.println(human);
             }
+        }
+        if(numberUser == 2) {
+            System.out.println("Это древо отсортировано в алфавитном порядке по имени: ");
+            System.out.println();
+
+            service.sortByName();
+            for (Human human : tree) {
+                    System.out.println(human);
+                }
+        }
+        if(numberUser == 3){
+            System.out.println("Это древо представленно в виде отсортированного по возрасту: ");
+            System.out.println();
+
+
         }
 
     }
