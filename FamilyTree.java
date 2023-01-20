fc878b9fe0b5149cae19f54aed2c988f45581937
Лилия <l_loshakova123@mail.ru>
2023-01-20 20:32:50 +0300

    New file

--- FamilyTree.java
+++ FamilyTree.java
@@ -3,35 +3,41 @@ package FamilyTree;
 import java.util.ArrayList;
 import java.util.Iterator;
 // import java.util.List;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class FamilyTree<T extends Human> implements Iterable<T> {
-    Scanner scan = new Scanner(System.in);
+    // Scanner scan = new Scanner(System.in);
+
     private ArrayList<T> treeHuman;
     private ArrayList<T> children;
 
-    public FamilyTree() { 
+    // Здесь я создаю два листа в которые вношу полный список людей и еще один
+    // список чисто с детьми
+    public FamilyTree() {
         treeHuman = new ArrayList<>();
         children = new ArrayList<>();
     }
+    
+    //Данный метод занимаетя сохранением людей в основной лист 
     public void addHuman(T human) {
         treeHuman.add(human);
     }
 
     // public void addHuman(T human) {
-    //     // Human human = new Human();
-    //     human.setIndex(scan.nextInt());
-    //     human.setName(scan.nextLine());
-    //     human.setAge(scan.nextInt());;
-    //     treeHuman.add(human);
-    //     scan.close();
-    // }
-    
-    // public void viewingList(T human, int index){
-    //     ((List<T>) human).get(index);
+    // // Human human = new Human();
+    // human.setIndex(scan.nextInt());
+    // human.setName(scan.nextLine());
+    // human.setAge(scan.nextInt());;
+    // treeHuman.add(human);
+    // scan.close();
     // }
 
-    public void addCild(T child){
+    // public void viewingList(T human, int index){
+    // ((List<T>) human).get(index);
+    // }
+
+    //Этот метод занимается сохранением детей в лист с детьми 
+    public void addCild(T child) {
         children.add(child);
     }
 
@@ -40,7 +46,7 @@ public class FamilyTree<T extends Human> implements Iterable<T> {
         return new FamilyTreeIterator<T>(treeHuman);
     }
 
-
+    //Так как листы приватные, то для их чттения нужны get 
     public ArrayList<T> getTreeHuman() {
         return treeHuman;
     }
