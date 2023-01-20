4be9940a0fc3fd4b86e16e7812b9a19941b11f52
Лилия <l_loshakova123@mail.ru>
2023-01-20 17:09:10 +0300

    New file

--- FamilyTree.java
+++ FamilyTree.java
@@ -19,6 +19,20 @@ public class FamilyTree<T extends Human> implements Iterable<T> {
         // while (scan.hasNextLine()) {
         //     treeHuman.add(scan.nextLine());
         // }
+        
+
+    }
+    public void add(){
+        Human h1 = new Human(1, "Мария", 55);
+        addHuman(h1);
+        Human h2 = new Human(2, "Валерий", 58);
+        addHuman(h2);
+        Human h3 = new Human(3, "Дмитрий", 30, h2, h1);
+        tree.addHuman(h3);
+        Human h4 = new Human(4, "Дарья", 35);
+        tree.addHuman(h4);
+        Human h5 = new Human(5, "София", 10, h3, h4, h2, h1);
+        tree.addHuman(h5);
     }
 
     public void addCild(T child){
