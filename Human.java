a825b304e983560c982d01e838d58571be64600d
Лилия <l_loshakova123@mail.ru>
2023-01-20 20:39:23 +0300

    Sketch

--- Human.java
+++ Human.java
@@ -10,12 +10,15 @@ public class Human implements Comparable<Human> {
     private Human grandmather;
     FamilyTree<Human> tree = new FamilyTree<>();
 
+    // Это модель для людей, у которых родители не указаны или которых нет в списке
     public Human(int index, String name, int age) {
         this.index = index;
         this.name = name;
         this.age = age;
     }
 
+    // Данная модель для людей, у которых есть родители, но нет или не указаны
+    // бабушка с дедушкой
     public Human(int index, String name, int age, Human father, Human mather) {
         this.index = index;
         this.name = name;
@@ -31,6 +34,8 @@ public class Human implements Comparable<Human> {
         }
     }
 
+    // Модель для людей у которых есть и мама с папой и бабушка с дедушкой(Можно еще
+    // разграничить сделать баушку с дедушкой по маменой и по папиной линии)
     public Human(int index, String name, int age, Human father, Human mather, Human grandfather, Human grandmather) {
         this.index = index;
         this.name = name;
@@ -41,6 +46,7 @@ public class Human implements Comparable<Human> {
         this.grandmather = grandmather;
     }
 
+    //Это метод для вывода древа пользователю 
     @Override
     public String toString() {
         if (father == null & mather == null) {
@@ -51,11 +57,11 @@ public class Human implements Comparable<Human> {
                     + this.father.name + "\n"
                     + " Мать: " + this.mather.name;
         } else {
-            return "№" + this.index + ". " + this.name + " - " + this.age + " лет: " + "\n" 
-            + " Отец: " + this.father.name + "\n" 
-            + " Мать: " + this.mather.name + "\n" 
-            + " Дедушка: " + this.grandfather.name + "\n" 
-            + " Бабушка: " + this.grandmather.name;
+            return "№" + this.index + ". " + this.name + " - " + this.age + " лет: " + "\n"
+                    + " Отец: " + this.father.name + "\n"
+                    + " Мать: " + this.mather.name + "\n"
+                    + " Дедушка: " + this.grandfather.name + "\n"
+                    + " Бабушка: " + this.grandmather.name;
         }
     }
 
@@ -64,6 +70,7 @@ public class Human implements Comparable<Human> {
         return name.compareTo(o.getName());
     }
 
+    // Так как переменные приватные, то здесь их get и set 
     public int getIndex() {
         return index;
     }
