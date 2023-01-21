b24ac47709b79be393b320036beb7714676fd0fd
Лилия <l_loshakova123@mail.ru>
2023-01-21 15:56:21 +0300

    Sketch

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package FamilyTree;
 
 public class Main {
-    //Метод, который запускает программу 
+    //Метод, который запускает программу/
     public static void main(String[] args) {
         View view = new View();
         view.communicationUse();
