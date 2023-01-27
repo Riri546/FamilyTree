de216f4e32db91e8d1f58bc9ee54a144bd42b6d9
Лилия <l_loshakova123@mail.ru>
2023-01-28 02:54:45 +0300

    Sketch

--- View.java
+++ View.java
@@ -9,7 +9,6 @@ public class View {
     Scanner scanner = new Scanner(System.in);
     int i = 0;
 
-
     public void communicationUse() {
         System.out.println("Здравствуйте! Это генеологическое древо.");
         System.out.println(
@@ -25,50 +24,53 @@ public class View {
         int numberUser = scanner.nextInt();
         System.out.println();
 
-        //Проверяем, какую цифру выбрал пользователь и выдаем нужный ответ 
+        // Проверяем, какую цифру выбрал пользователь и выдаем нужный ответ
         if (numberUser == 1) {
             System.out.println("Пожалуйста! Как вы и просили список людей из семейного древа: ");
             contr.withoutSorting();
-        }
-        else if (numberUser == 2) {
+        } else if (numberUser == 2) {
             System.out.println("Это древо отсортировано в алфавитном порядке по имени: ");
             contr.treeByName();
-        }
-        else if (numberUser == 3) {
+        } else if (numberUser == 3) {
             System.out.println("Это древо представленно в виде отсортированного по возрасту: ");
             contr.treeByAge();
         }
         tree.getTreeHuman().size();
-        //  нужно изменить подход
+        // нужно изменить подход
         if (numberUser == 4) {
             System.out.print("Введите порядковый номер члена этой семьи: ");
             int indexFamily = scanner.nextInt();
-            //Пробую перенести добавление персов в другой файл 
-            for(i = 0; i <= tree.getTreeHuman().size(); i++){
+            // Пробую перенести добавление персов в другой файл
+            for (i = 0; i <= tree.getTreeHuman().size();) {
                 tree.myHuman(null);
-                System.out.println(tree.getTreeHuman().get(indexFamily));
+                System.out.println(tree.getTreeHuman().get(indexFamily - 1));
+                break;
             }
+            if (i > tree.getTreeHuman().size()) {
+                System.out.println("К сожалению в списке нет такого номера");
+            }
+
             // if (indexFamily == 1) {
-            //     System.out.println(a.myHuman());
+            // System.out.println(a.myHuman());
             // }
 
             // if (indexFamily == 1) {
-            //     System.out.println(contr.m);
+            // System.out.println(contr.m);
             // }
             // else if (indexFamily == 2) {
-            //     System.out.println(h2);
+            // System.out.println(h2);
             // }
             // else if (indexFamily == 3) {
-            //     System.out.println(h3);
+            // System.out.println(h3);
             // }
             // else if (indexFamily == 4) {
-            //     System.out.println(h4);
+            // System.out.println(h4);
             // }
             // else if (indexFamily == 5) {
-            //     System.out.println(h5);
+            // System.out.println(h5);
             // }
             // else if (indexFamily >= 6) {
-            //     System.out.println("К сожалению в списке нет такого номера");
+            // System.out.println("К сожалению в списке нет такого номера");
             // }
         }
 
