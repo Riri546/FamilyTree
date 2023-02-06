de362677972f396bb074abd688351bfa7a1a36ab
Лилия <l_loshakova123@mail.ru>
2023-02-07 01:38:46 +0300

    Delete file

--- h.java
+++ h.java
@@ -1,5 +1,25 @@
 package FamilyTree;
 
 public class h {
+    FamilyTree<Human> tree = new FamilyTree<>();
+    Service service = new Service(tree);
     
+
+    public void treeByName() {
+        tree.myHuman(null);
+        service.sortByName();
+
+        for (Human human : tree) {
+            System.out.println(human);
+        }
+    }
+
+    public void treeByAge() {
+        tree.myHuman(null);
+        service.sortByAge();
+
+        for (Human human : tree) {
+            System.out.println(human);
+        }
+    }
 }
