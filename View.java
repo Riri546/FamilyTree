7785ec773e760768fece44285bc8db735c25b5ec
Лилия <l_loshakova123@mail.ru>
2023-02-07 02:04:56 +0300

    Sketch

--- View.java
+++ View.java
@@ -1,48 +1,6 @@
 package FamilyTree;
 
-import java.util.Scanner;
-
 // Метод для общения с пользователем 
-public class View {
-    Controller contr = new Controller();
-    Presenter pres = new Presenter();
-    Scanner scanner = new Scanner(System.in);
-    int i = 0;
-
-    public void communicationUse() {
-        System.out.println("Здравствуйте! Это генеологическое древо.");
-        System.out.println(
-                "В каком порядке хотите просмотреть список людей?" + "\n" +
-                        "Введите нужную цифру для просмотра: " + "\n" +
-                        "1 - Вывести полный список людей из семейного древа" + "\n" +
-                        "2 - Отсортировать полный список по имени в алфавитном порядке" + "\n" +
-                        "3 - Отсортировать по возросту в порядке возрастания" + "\n" +
-                        "4 - Вывести одного человека и его родственников");
-
-        System.out.println();
-        System.out.print("Введите цифру для выполнения действия: ");
-        int numberUser = scanner.nextInt();
-        System.out.println();
-
-        // Проверяем, какую цифру выбрал пользователь и выдаем нужный ответ
-        if (numberUser == 1) {
-            System.out.println("Пожалуйста! Как вы и просили список людей из семейного древа: ");
-            contr.withoutSorting();
-        } else if (numberUser == 2) {
-            System.out.println("Это древо отсортировано в алфавитном порядке по имени: ");
-            pres.treeByName();
-        } else if (numberUser == 3) {
-            System.out.println("Это древо представленно в виде отсортированного по возрасту: ");
-            pres.treeByAge();
-        }
-        if (numberUser == 4) {
-            System.out.print("Введите порядковый номер члена этой семьи: ");
-            int indexFamily = scanner.nextInt();
-            contr.searchPerson(indexFamily);
-        }
-        else if (numberUser >= 5) {
-            System.out.println("Похоже, вы что-то ввели неверно");
-        }
-        scanner.close();
-    }
+public interface View {
+    void communicationUse();
 }
