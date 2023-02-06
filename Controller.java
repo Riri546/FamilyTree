ef325bbcca56d01394b0be517cb4aaa446943b4f
Лилия <l_loshakova123@mail.ru>
2023-02-07 01:44:24 +0300

    Sketch

--- Controller.java
+++ Controller.java
@@ -2,7 +2,6 @@ package FamilyTree;
 
 public class Controller {
     FamilyTree<Human> tree = new FamilyTree<>();
-    // Service service = new Service(tree);
     
     public void withoutSorting() {
         tree.myHuman(null);
@@ -10,24 +9,6 @@ public class Controller {
             System.out.println(human);
         }
     }
-
-    // public void treeByName() {
-    //     tree.myHuman(null);
-    //     service.sortByName();
-
-    //     for (Human human : tree) {
-    //         System.out.println(human);
-    //     }
-    // }
-
-    // public void treeByAge() {
-    //     tree.myHuman(null);
-    //     service.sortByAge();
-
-    //     for (Human human : tree) {
-    //         System.out.println(human);
-    //     }
-    // }
 
     public void searchPerson(int idFamily){
         tree.myHuman(null);
