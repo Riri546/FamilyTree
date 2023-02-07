c446b665547b94986c66bb2ee97f71bdcbe9ecc2
Лилия <l_loshakova123@mail.ru>
2023-02-07 04:12:01 +0300

    Sketch

--- Recorded.java
+++ Recorded.java
@@ -1,16 +1,15 @@
 package FamilyTree;
 
 import java.io.*;
-import java.util.ArrayList;
 
-public class Recorded implements Serializable {
-
-  
+public class Recorded {
     public static void main(String[] args) throws FileNotFoundException {
         FamilyTree tree = new FamilyTree<Human>();
         String tmp = tree.getTreeHuman().toString();
         PrintWriter pw = new PrintWriter(new FileOutputStream("file"));
-        pw.println(tmp.substring(1, tmp.length() - 1));
+        // pw.println(tmp.substring(1, tmp.length() - 1));
+        // pw.println(tmp);
+        pw.write(tmp.substring(1, tmp.length() - 1));
 
     }
 }
