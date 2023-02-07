ebc14b60392c425e636df8e51b0a1409f3efecee
Лилия <l_loshakova123@mail.ru>
2023-02-07 03:05:43 +0300

    Sketch

--- Record.java
+++ Record.java
@@ -1,5 +1,24 @@
 package FamilyTree;
 
+import java.io.*;
+
 public class Record {
-    
+    public static void main(String[] args) {
+        
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+           // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+             
+            writer.flush();
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        } 
+    } 
 }
