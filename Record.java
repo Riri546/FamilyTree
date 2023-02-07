b4003ecd5900e6dfc1aaf069bd230e848618ab57
Лилия <l_loshakova123@mail.ru>
2023-02-07 03:24:33 +0300

    Sketch

--- Record.java
+++ Record.java
@@ -1,24 +1,32 @@
 package FamilyTree;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Record {
+    
+
     public static void main(String[] args) {
+        FamilyTree<Human> tree = new FamilyTree<Human>();
+        Path out = Paths.get("output.txt");
+        List<Human> arrayList = new ArrayList<>((tree.getTreeHuman()));
         
-        try(FileWriter writer = new FileWriter("notes3.txt", false))
-        {
-           // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-             
-            writer.flush();
-        }
-        catch(IOException ex){
-             
-            System.out.println(ex.getMessage());
-        } 
-    } 
+
+        Files.write(out, arrayList, Charset.defaultCharset());
+        // // запись всей строки
+        // String text = "Hello Gold!";
+        // writer.write(text);
+        // // запись по символам
+        // writer.append('\n');
+        // writer.append('E');
+
+        // writer.flush();
+    }
 }
