62189e3c922ee7bbd4fe46b9b1c3c8ebc5309994
Лилия <l_loshakova123@mail.ru>
2023-02-07 03:57:52 +0300

    Sketch

--- Recorded.java
+++ Recorded.java
@@ -3,33 +3,27 @@ package FamilyTree;
 import java.io.*;
 
 public class Recorded implements Serializable {
-    private String name;
-    private int age;
-    private String contact;
+    // private String name;
+    // private int age;
+    // private String contact;
+    Human human;
+    FamilyTree tree = new FamilyTree<Human>();
 
-    public Recorded(String name, int age, String contact) {
-        this.name = name;
-        this.age = age;
-        this.contact = contact;
+    public Recorded(Human human) {
+        
     }
 
     @Override
     public String toString() {
-        return "Древо{" +
-                "Имя ='" + name + '\'' +
-                ", Возрост ='" + age + '\'' +
-                ", Кем является ='" + contact + '\'' +
-                '}';
+        // return human.toString();
+        return tree.getTreeHuman();
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Recorded h1 = new Recorded("Коля", 24, "Сын Даши и Вити");
-        Recorded h2 = new Recorded("Даша", 53, "Мать Коли");
-        Recorded h3 = new Recorded("Витя", 65, "Отец Коли");
+        
+        Recorded h1 = new Recorded();
 
         System.out.println(h1);
-        System.out.println(h2);
-        System.out.println(h3);
     }
 }
 
